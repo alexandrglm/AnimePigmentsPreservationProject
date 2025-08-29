c43ae124a57a89ea0a472e94730abda723642111
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-08-29 18:59:15 +0200

    Fix Krita .kpl format completion. Use krita sRGB Elle v2 default space colour insted of v4 for compatibility

--- engine/plugin_palettes/v0.1/export_krita.py
+++ engine/plugin_palettes/v0.1/export_krita.py
@@ -6,15 +6,18 @@ from pathlib import Path
 
 
 """
-KRITA .kpl (XML-alike) module exporter v0.1
+KRITA .kpl (XML-alike) module exporter v0.2
 
 Check custom fields like:
 
 - Custom ICC importer -> y/N -> y: hHandles custom ICC profiles
 
+
+Retaked:
+- Embebbed ICC profile MUST follow krita's default specs! (d0ne)
 """
 
-def export_krita(colours, output_directory="palette_kpl", icc_profile='sRGB', icc_file_path=None):
+def export_krita(colours, output_directory="palette_kpl", icc_file_path=None):
     """
 
     Paths/VArs:
@@ -22,75 +25,83 @@ def export_krita(colours, output_directory="palette_kpl", icc_profile='sRGB', ic
     - icc_profile -> display name for the profile in XML (ignored if icc_file_path provided)
     - icc_file_path -> optional path to a custom ICC file to embed
     """
-
     palettes = {}
 
-    for colour in colours:
-        palettes.setdefault(colour["chart"], []).append(colour)
+    for col in colours:
+        palettes.setdefault(col['chart'], []).append(col)
 
     os.makedirs(output_directory, exist_ok=True)
 
 
 
-    for chart, colour_list in palettes.items():
-        filename = Path(output_directory) / f"{chart.replace(' ', '_')}.kpl"
+    for chart, clist in palettes.items():
+        filepath = Path(output_directory) / f"{chart.replace(' ', '_')}.kpl"
+        with zipfile.ZipFile(filepath, 'w') as kpl:
 
 
-        with zipfile.ZipFile(filename, 'w') as kpl_file:
+            # 1. MUST BE mimetype
+            kpl.writestr('mimetype', 'application/x-krita-palette')
 
-            # krita custom mimetype from docs
-            kpl_file.writestr('mimetype', 'application/x-krita-palette')
-
-            # MUST BE colorset.xml
-            colour_set_xml = ET.Element('ColorSet',
-                rows=str(len(colour_list)//8+1),
-                columns='8',
-                name=chart,
-                version='2.0')
-
-            for colour in colour_list:
-                colour_entry = ET.SubElement(colour_set_xml, 'ColorEntry')
-                colour_entry.set('name', colour['code'])
-                colour_entry.set('rgb', f"{colour['red']},{colour['green']},{colour['blue']}")
-
-            colour_set_xml_string = ET.tostring(colour_set_xml, encoding='utf-8', method='xml')
-            kpl_file.writestr('colorset.xml', colour_set_xml_string)
+            # 2. MUST BE colorset.xml
+            rows = str((len(clist) + 7) // 8)
+            cs = ET.Element('ColorSet', {
+                'name': chart,
+                'version': '2.0',
+                'columns': '8',
+                'rows': rows,
+                'readonly': 'false'
+            })
 
 
+            for c in clist:
+                e = ET.SubElement(cs, 'ColorSetEntry', {
+                    'name': c['code'],
+                    'id': c['code'],
+                    'bitdepth': 'F32',
+                    'spot': 'false'
+                })
+                rgb = ET.SubElement(e, 'RGB', {
+                    'r': str(c['red']/255.0),
+                    'g': str(c['green']/255.0),
+                    'b': str(c['blue']/255.0)
+                })
+            kpl.writestr('colorset.xml', ET.tostring(cs, encoding='utf-8'))
 
-            # ICC PATH or NOT
+
+            # 3. MUST BE profiles.xml
+            """
+            MUST INCLUDE THE ICC PROFILE USED INFO AND OR DEFAULT!
+            Otherwise .kpl palette won't work
+            """
             if icc_file_path and os.path.isfile(icc_file_path):
-                icc_filename = Path(icc_file_path).name
                 icc_name = Path(icc_file_path).stem
-
+                icc_filename = Path(icc_file_path).name
 
             else:
-                icc_filename = f'{icc_profile}.icc'
-                icc_name = icc_profile
+                icc_name = 'sRGB'
+                icc_filename = 'sRGB.icc'
 
-            # MUST BE profiles.xml
-            profiles_xml = ET.Element('Profiles')
-            profile = ET.SubElement(profiles_xml, 'Profile',
-                name=icc_name,
-                filename=icc_filename,
-                colorModelId='RGBA',
-                colorDepthId='F32')
-            profiles_xml_string = ET.tostring(profiles_xml, encoding='utf-8', method='xml')
-            kpl_file.writestr('profiles.xml', profiles_xml_string)
+            profiles = ET.Element('Profiles')
+
+            prof = ET.SubElement(profiles, 'Profile', {
+                'name': icc_name,
+                'filename': icc_filename,
+                'colorModelId': 'RGBA',
+                'colorDepthId': 'F32'
+            })
+
+            kpl.writestr('profiles.xml', ET.tostring(profiles, encoding='utf-8'))
 
 
 
-            # CUSTOM ICC EMBEDDING
+            # 4. embed ICC file
+
             if icc_file_path and os.path.isfile(icc_file_path):
 
                 with open(icc_file_path, 'rb') as f:
-
-                    kpl_file.writestr(icc_filename, f.read())
-
+                    kpl.writestr(icc_filename, f.read())
 
             else:
+                kpl.writestr(icc_filename, b'')
 
-                kpl_file.writestr(icc_filename, b'')
-
-
-        print(f"[ D0NE! ] KPL file generated: {filename}")
+        print(f"[ D0NE! ] KPL file generated: {filepath}")
